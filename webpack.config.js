365e7fb206f837070f2cf2c64fe8f7631c48a4b0
dasosjt <stdjca@gmail.com>
2017-12-07 21:02:21 -0600

    change port webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,5 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
   ]
 };
